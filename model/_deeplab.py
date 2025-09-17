7f9fdecd466780ec3d3aadde24f6bad3f61e7c29
Josh Li <j234li@watgpu208.watgpu-domain.cs.uwaterloo.ca>
2025-09-17 23:07:40 +0000

    added readme

--- model/_deeplab.py
+++ model/_deeplab.py
@@ -1,2 +1,3 @@
 class DeepLabHeadV3Plus(nn.Module):
-    def __init__(self, in_channels, low_level_channels, num_classes, aspp_dilate)
+    def __init__(self, in_channels, low_level_channels, num_classes, aspp_dilate):
+        
